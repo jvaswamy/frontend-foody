e9a904b9a9ebe5fa6a15370e5a366a999955f4ba
jvaswamy <109134449+jvaswamy@users.noreply.github.com>
2026-06-13 18:34:13 +0530

    Update API_URL to new backend URL

--- src/data/apiPath.jsx
+++ src/data/apiPath.jsx
@@ -1,2 +1,2 @@
-export const API_URL = "https://foody-app-epre.onrender.com";
+export const API_URL = "https://backend-nodejs-foody-plp8.onrender.com";
 // export const API_URL = "http://localhost:4000";
